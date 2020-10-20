2d5b6ea846729eb578433033a188c9371ee7d895
Chetan Gupta <chetangupta@chetan.gupta>
2020-10-21 01:50:44 +0530

    added/dsl

--- app/src/main/java/com/example/awesomeextensions/ImageKtx.kt
+++ app/src/main/java/com/example/awesomeextensions/ImageKtx.kt
@@ -4,6 +4,7 @@ import android.widget.ImageView
 import androidx.annotation.DimenRes
 import androidx.annotation.DrawableRes
 import com.squareup.picasso.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 data class PicassoOption(
@@ -105,3 +106,87 @@ fun ImageView.loadImage(imageUrl: String?, picassoOption: PicassoOption) {
             .into(this@loadImage, picassoCallback)
     }
 }
+
+fun main(mainActivity: MainActivity) {
+    val image = mainActivity.image
+    picasso(imageView = image) {
+        imageUrl = ""
+    }
+}
+
+class PicassoDsl {
+    private val picasso = Picasso.get()
+
+    @DrawableRes
+    var errorHolder: Int = -1
+
+    @DrawableRes
+    var placeholder: Int = -1
+    var scaleType: PicassoScaleType = PicassoScaleType.Default
+    var cache: PicassoCache = PicassoCache.Default
+    var picassoCallback: Callback = Callback.EmptyCallback()
+    val enableDebug: Boolean = false
+    var imageUrl: String? = ""
+    fun build(): RequestCreator? {
+        //enables logging
+        picasso.isLoggingEnabled = enableDebug
+        //green (memory, best performance)
+        //blue (disk, good performance)
+        //red (network, worst performance)
+        picasso.setIndicatorsEnabled(enableDebug)
+        val picassoReqCreator = if (imageUrl.isNullOrBlank()) {
+            picasso.load(errorHolder)
+        } else {
+            picasso.load(imageUrl)
+        }
+        picassoReqCreator.also {
+            when (scaleType) {
+                is PicassoScaleType.Default -> {
+                    //do nothing
+                }
+                is PicassoScaleType.CenterCrop -> it.fit().centerCrop()
+                is PicassoScaleType.Fit -> it.fit()
+                is PicassoScaleType.CenterInside -> it.centerInside()
+                is PicassoScaleType.Custom -> with(scaleType) {
+                    val (height, width) = this as PicassoScaleType.Custom
+                    it.resize(height, width)
+                }
+                is PicassoScaleType.CustomDimen -> with(scaleType) {
+                    val (height, width) = this as PicassoScaleType.CustomDimen
+                    it.resizeDimen(height, width)
+                }
+            }
+        }
+            .also {
+                when (cache) {
+                    PicassoCache.Default -> {
+                        //do nothing
+                    }
+                    PicassoCache.ForceLRU -> {
+                        it.networkPolicy(NetworkPolicy.OFFLINE)
+                    }
+                    PicassoCache.Disk -> {
+                        it.memoryPolicy(MemoryPolicy.NO_CACHE)
+                    }
+                    PicassoCache.ForceDisk -> {
+                        it.memoryPolicy(MemoryPolicy.NO_CACHE)
+                        it.networkPolicy(NetworkPolicy.OFFLINE)
+                    }
+                    PicassoCache.ForceNetwork -> {
+                        it.memoryPolicy(MemoryPolicy.NO_CACHE, MemoryPolicy.NO_STORE)
+                        it.networkPolicy(NetworkPolicy.NO_CACHE)
+                    }
+                }
+            }
+            .error(errorHolder)
+            .also { if (placeholder != -1) it.placeholder(placeholder) }
+
+        return picassoReqCreator
+    }
+}
+
+fun picasso(imageView: ImageView, initializer: PicassoDsl.() -> Unit) {
+    val picasso = PicassoDsl().apply(initializer).build()
+    picasso?.into(imageView)
+}
+
